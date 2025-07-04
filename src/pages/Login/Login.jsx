664af0724217a9f382fc7405b81fd4fbe9811067
DIVA <Nolsatubeat@gmail.com>
2025-07-04 08:51:08 +0800

    asd

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Masuk ke Akun Anda
+            Masuk ke Akun Anda ya
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Belum punya akun?{' '}
